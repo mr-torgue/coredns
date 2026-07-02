d4eb895a7f6d74d6ed155a918f459a3c549a3b45
mr_torgue <me+github@folmer.info>
2026-07-02 11:15:26 +1000

    split go:linkname in decleration and initialisation

--- coredns.go
+++ coredns.go
@@ -17,19 +17,21 @@ import (
 // AES128 should be quantum-safe, but AES256 might be more secure.
 
 //go:linkname defaultCipherSuitesTLS13 crypto/tls.defaultCipherSuitesTLS13
-var defaultCipherSuitesTLS13 = []uint16{
-	tls.TLS_AES_256_GCM_SHA384,
-	//tls.TLS_AES_128_GCM_SHA256,
-	tls.TLS_CHACHA20_POLY1305_SHA256,
-}
+var defaultCipherSuitesTLS13 []uint16
 
 //go:linkname defaultCipherSuitesTLS13NoAES crypto/tls.defaultCipherSuitesTLS13NoAES
-var defaultCipherSuitesTLS13NoAES = []uint16{
-	tls.TLS_CHACHA20_POLY1305_SHA256,
-	tls.TLS_AES_256_GCM_SHA384,
-	//tls.TLS_AES_128_GCM_SHA256,
-}
+var defaultCipherSuitesTLS13NoAES []uint16
 
 func main() {
+	defaultCipherSuitesTLS13 = []uint16{
+		tls.TLS_AES_256_GCM_SHA384,
+		//tls.TLS_AES_128_GCM_SHA256,
+		tls.TLS_CHACHA20_POLY1305_SHA256,
+	}
+	defaultCipherSuitesTLS13NoAES = []uint16{
+		tls.TLS_CHACHA20_POLY1305_SHA256,
+		tls.TLS_AES_256_GCM_SHA384,
+		//tls.TLS_AES_128_GCM_SHA256,
+	}
 	coremain.Run()
 }
