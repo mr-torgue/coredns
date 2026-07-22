9813fc321e29ba3298dd36fd2ff201c53f2de1e1
mr_torgue <me+github@folmer.info>
2026-07-22 22:42:38 +1000

    added proto to scrubwriter
    Signed-off-by: mr_torgue <me+github@folmer.info>

--- request/writer.go
+++ request/writer.go
@@ -2,7 +2,9 @@ package request
 
 import (
 	"crypto/tls"
+	"net"
 
+	clog "github.com/coredns/coredns/plugin/pkg/log"
 	"github.com/miekg/dns"
 )
 
@@ -35,4 +37,20 @@ func (s *ScrubWriter) ConnectionState() *tls.ConnectionState {
 		return cs.ConnectionState()
 	}
 	return nil
+}
+
+// Proto gets the protocol used as the transport. This will be udp or tcp.
+func (s *ScrubWriter) Proto() string {
+	clog.Infof("ResponseWriter(scrub) type: %T", s.ResponseWriter)
+	// return Write.Proto(), if it is implemented
+	if protoProvider, ok := s.ResponseWriter.(interface{ Proto() string }); ok {
+		return protoProvider.Proto()
+	}
+	if _, ok := s.ResponseWriter.RemoteAddr().(*net.UDPAddr); ok {
+		return "udp"
+	}
+	if _, ok := s.ResponseWriter.RemoteAddr().(*net.TCPAddr); ok {
+		return "tcp"
+	}
+	return "udp"
 }
