3b22dac49549779e299afc7513884cc0d34f610c
mr_torgue <me+github@folmer.info>
2026-07-22 22:07:41 +1000

    more debugging
    Signed-off-by: mr_torgue <me+github@folmer.info>

--- plugin/pkg/edns/edns.go
+++ plugin/pkg/edns/edns.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"sync"
 
+	clog "github.com/coredns/coredns/plugin/pkg/log"
 	"github.com/miekg/dns"
 )
 
@@ -61,6 +62,7 @@ func Version(req *dns.Msg) (*dns.Msg, error) {
 
 // Size returns a normalized size based on proto.
 func Size(proto string, size uint16) uint16 {
+	clog.Infof("proto: %s, size: %d", proto, size)
 	if proto == "tcp" || proto == "quic" {
 		return dns.MaxMsgSize
 	}
